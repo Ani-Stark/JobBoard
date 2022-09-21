2010c7eb73d31f598f06985c45f9d35bb45af28f
Ani-Stark <taniyamishra55555@gmail.com>
2022-09-21 22:20:59 +0530

    third commit

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -8,9 +8,10 @@ function Home() {
     
   return (
     <>
-      <nav>
-        JOB BOARD
-      </nav>
+    <div class="board">
+        <h1>JOB BOARD</h1>
+      </div>
+
       <div class="container">
         <div class="inner-width">
           <div class="jobs">
@@ -21,7 +22,7 @@ function Home() {
                   <span class="category">Photoshop</span>
                   <span class="new">New</span>
                   <span class="featured">Featured</span>
-                  </div>
+                </div>
                 <h3 class="title">Frontend Developer</h3>
                 <p class="properties">
                   <spam class="timestamp">5 Mar 2021</spam>
@@ -41,21 +42,20 @@ function Home() {
 
               <div class="job-header">
                 <div class="job-category">
-                  <span class="category">Photoshop</span>
+                  <span class="category">Web Development</span>
                   <span class="new">New</span>
                   <span class="featured">Featured</span>
                   </div>
-                <h3 class="title">Frontend Developer</h3>
+                <h3 class="title">Full Stack Developer</h3>
                 <p class="properties">
-                  <spam class="timestamp">5 Mar 2021</spam>
+                  <spam class="timestamp">2 July 2022</spam>
                   <span class="type">Contract</span>
-                  <span class="country">USA only</span>
+                  <span class="country">New York</span>
                 </p>
               </div>
             </div>
             <div class="tags">
-              <span>HTML</span>
-              <span>CSS</span>
+              <span>Bootstrap</span>
               <span>JavaScript</span>
             </div>
           </div>
@@ -64,20 +64,65 @@ function Home() {
 
               <div class="job-header">
                 <div class="job-category">
-                  <span class="category">Photoshop</span>
+                  <span class="category">UX/UI</span>
                 
                   </div>
-                <h3 class="title">Frontend Developer</h3>
+                <h3 class="title">Graphic Designer</h3>
                 <p class="properties">
-                  <spam class="timestamp">5 Mar 2021</spam>
+                  <spam class="timestamp">3 April 2023</spam>
                   <span class="type">Contract</span>
                   <span class="country">USA only</span>
                 </p>
               </div>
             </div>
             <div class="tags">
-              <span>HTML</span>
-              <span>CSS</span>
+              <span>Figma</span>
+              <span>Adobe XD</span>
+            </div>
+          </div>
+          <div class="jobs">
+            <div class="header">
+
+              <div class="job-header">
+                <div class="job-category">
+                  <span class="category">Big Data</span>
+                  <span class="new">New</span>
+                  <span class="featured">Featured</span>                 
+
+                  </div>
+                <h3 class="title">Data Analyst</h3>
+                <p class="properties">
+                  <spam class="timestamp">5 Aug 2021</spam>
+                  <span class="type">Contract</span>
+                  <span class="country">California</span>
+                </p>
+              </div>
+            </div>
+            <div class="tags">
+              <span>MySQL</span>
+              <span>Oracle</span>
+            </div>
+          </div>
+          <div class="jobs">
+            <div class="header">
+
+              <div class="job-header">
+                <div class="job-category">
+                  <span class="category">AI/ML</span>
+                  <span class="new">New</span>
+                  <span class="featured">Featured</span>                 
+                  </div>
+                <h3 class="title">Backend Developer</h3>
+                <p class="properties">
+                  <spam class="timestamp">25 May 2023</spam>
+                  <span class="type">Contract</span>
+                  <span class="country">London</span>
+                </p>
+              </div>
+            </div>
+            <div class="tags">
+              <span>Python</span>
+              <span>Java</span>
               <span>JavaScript</span>
             </div>
           </div>
@@ -86,63 +131,21 @@ function Home() {
 
               <div class="job-header">
                 <div class="job-category">
-                  <span class="category">Photoshop</span>
-            
+                  <span class="category">Big Data</span>
+                  <span class="new">New</span>
+                  <span class="featured">Featured</span>
                   </div>
-                <h3 class="title">Frontend Developer</h3>
+                <h3 class="title">Data Scientist</h3>
                 <p class="properties">
-                  <spam class="timestamp">5 Mar 2021</spam>
+                  <spam class="timestamp">31 Oct 2022</spam>
                   <span class="type">Contract</span>
-                  <span class="country">USA only</span>
+                  <span class="country">South Wales</span>
                 </p>
               </div>
             </div>
             <div class="tags">
-              <span>HTML</span>
-              <span>CSS</span>
-              <span>JavaScript</span>
-            </div>
-          </div>
-          <div class="jobs">
-            <div class="header">
-
-              <div class="job-header">
-                <div class="job-category">
-                  <span class="category">Photoshop</span>
-                  
-                  </div>
-                <h3 class="title">Frontend Developer</h3>
-                <p class="properties">
-                  <spam class="timestamp">5 Mar 2021</spam>
-                  <span class="type">Contract</span>
-                  <span class="country">USA only</span>
-                </p>
-              </div>
-            </div>
-            <div class="tags">
-              <span>HTML</span>
-              <span>CSS</span>
-              <span>JavaScript</span>
-            </div>
-          </div>
-          <div class="jobs">
-            <div class="header">
-
-              <div class="job-header">
-                <div class="job-category">
-                  <span class="category">Photoshop</span>
-                  </div>
-                <h3 class="title">Frontend Developer</h3>
-                <p class="properties">
-                  <spam class="timestamp">5 Mar 2021</spam>
-                  <span class="type">Contract</span>
-                  <span class="country">USA only</span>
-                </p>
-              </div>
-            </div>
-            <div class="tags">
-              <span>HTML</span>
-              <span>CSS</span>
+              <span>MySQL</span>
+              <span>Oracle</span>
               <span>JavaScript</span>
               <span>Python</span>
             </div>
